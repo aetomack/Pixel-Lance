d04e7e5b9b14410f75b7da19e1e9d4b0824889e8
marcumr1 <122474950+marcumr1@users.noreply.github.com>
2025-04-21 12:43:46 -0500

    Basic Game Loop Implemented

--- pixel-lance/scripts/baby_gronk.gd
+++ pixel-lance/scripts/baby_gronk.gd
@@ -3,20 +3,56 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var attacking = false
+
+signal colliding(colliding_body,attacking)
 
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var horizontal := Input.get_axis("ui_left", "ui_right")
+	var vertical := Input.get_axis("ui_up","ui_down")
+	if horizontal or vertical: # if moving
+		var direction = Vector2(horizontal,vertical)
+		direction = direction.normalized()
+		velocity = direction * SPEED
+		# Set animation
+		if not attacking:
+			$AnimatedSprite2D.animation = "moving"
+			if horizontal: # change direction only if moving
+				$AnimatedSprite2D.flip_h = velocity.x < 0 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		if not attacking:
+			$AnimatedSprite2D.animation = "idle"
+	# Attacking
+	if Input.is_action_just_pressed("ui_select"):
+		attack()
+	
 	move_and_slide()
+
+# to use for fighting
+func facing_left():
+	return $AnimatedSprite2D.flip_h
+
+func facing_right():
+	return not $AnimatedSprite2D.flip_h	
+
+func attack():
+	attacking = true
+	$AnimatedSprite2D.animation = "attack"
+	$AttackTimer.start()
+
 
 
 func _on_battle_box_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
-		body.attack_player()
+		# print("colliding with ",body.name)
+		colliding.emit(body,attacking)
+		
+
+
+func _on_attack_timer_timeout() -> void:
+	attacking = false
--- pixel-lance/scripts/game.gd
+++ pixel-lance/scripts/game.gd
@@ -5,15 +5,16 @@ var enemy := preload("res://scenes/enemy.tscn")
 @onready var path_follow_2d: PathFollow2D = $Player/Path2D/PathFollow2D
 var wave = 0
 var enemy_count = 0
-var max_enemy
+var enemies_this_wave
+var enemies_spawned_this_wave = 0
+var num_waves = 2
 
 func _ready():
-	var instance = enemy.instantiate()
-	add_child(instance)
-	max_enemy = (2*wave) + 4
-	
+	start_game()
+
+# Spawn enemies for this wave
 func _on_spawn_timer_timeout() -> void:
-	while(enemy_count < max_enemy):
+	if(enemies_spawned_this_wave < enemies_this_wave):
 		var rng = RandomNumberGenerator.new()
 		
 		rng.randomize
@@ -23,6 +24,44 @@ func _on_spawn_timer_timeout() -> void:
 		instance.position = $Player/Path2D/PathFollow2D/Marker2D.global_position
 		game_root.add_child(instance)
 		enemy_count = enemy_count + 1
-	wave+=1
+		
+		enemies_spawned_this_wave += 1
+	else:
+		$SpawnTimer.stop()
+
+func new_wave():
+	print("new_wave, wave = ",wave)
+	enemies_this_wave = (2*wave) + 4
+	enemies_spawned_this_wave = 0
+	$SpawnTimer.start()
+
+func start_game():
+	$Player.position = $Player/Path2D/PathFollow2D/Marker2D.position
+	wave = 0
+	enemy_count = 0
+	new_wave()
 	
+	
+func you_win():
+	print("You Win!")
+	
+func _process(delta: float) -> void:
+	pass
+
+func check_game_state() -> void:
+	# if all enemies have been defeated
+	if enemies_spawned_this_wave == enemies_this_wave and enemy_count == 0:
+		wave += 1
+		if (wave == num_waves):
+			you_win()
+		else:
+			new_wave()
+
+func _on_player_colliding(colliding_body: Node2D, attacking: bool) -> void:
+	print("colliding!")
+	if attacking: # If the player is attacking during the collision, remove the enemy
+		var enemy_node = colliding_body.get_parent()
+		enemy_node.queue_free()
+		enemy_count -= 1
+		check_game_state()
 	
